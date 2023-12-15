f836e26dfa550aac52588670c483d2e4bebafaac
DAY280130 <dimas.280130@gmail.com>
2023-12-16 01:07:08 +0700

    fix: changed import source

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
-import { initializeApp, credential, type FirebaseError } from "firebase-admin";
+import { cert, initializeApp, type FirebaseError } from "firebase-admin/app";
 
 export const firebaseApp = initializeApp({
-  credential: credential.cert({
+  credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
