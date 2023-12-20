e0e82903c94d413557602ed0bc83f9c673ad2a51
DAY280130 <93344026+day280130@users.noreply.github.com>
2023-12-20 11:50:39 +0700

    feat: added admin check to food and exercise mutation endpoints

--- src/services/exercise/router.ts
+++ src/services/exercise/router.ts
@@ -1,13 +1,19 @@
 import { authCheck } from "@/middlewares/auth";
 import { Router } from "express";
 import { exerciseHandlers } from "./handlers";
+import { adminCheck } from "@/middlewares/role";
 
 export const exerciseRouters = Router();
 
 const BASE_ENDPOINT = "/exercise";
 
-exerciseRouters.post(`${BASE_ENDPOINT}`, authCheck, exerciseHandlers.postExercise);
+exerciseRouters.post(`${BASE_ENDPOINT}`, authCheck, adminCheck, exerciseHandlers.postExercise);
 exerciseRouters.get(`${BASE_ENDPOINT}`, authCheck, exerciseHandlers.getExercises);
 exerciseRouters.get(`${BASE_ENDPOINT}/:id`, authCheck, exerciseHandlers.getExercise);
-exerciseRouters.put(`${BASE_ENDPOINT}/:id`, authCheck, exerciseHandlers.putExercise);
-exerciseRouters.delete(`${BASE_ENDPOINT}/:id`, authCheck, exerciseHandlers.deleteExercise);
+exerciseRouters.put(`${BASE_ENDPOINT}/:id`, authCheck, adminCheck, exerciseHandlers.putExercise);
+exerciseRouters.delete(
+  `${BASE_ENDPOINT}/:id`,
+  authCheck,
+  adminCheck,
+  exerciseHandlers.deleteExercise
+);
--- src/services/food/router.ts
+++ src/services/food/router.ts
@@ -1,13 +1,14 @@
 import { authCheck } from "@/middlewares/auth";
 import { Router } from "express";
 import { foodHandlers } from "./handlers";
+import { adminCheck } from "@/middlewares/role";
 
 export const foodRouters = Router();
 
 const BASE_ENDPOINT = "/food";
 
-foodRouters.post(`${BASE_ENDPOINT}`, authCheck, foodHandlers.postFood);
+foodRouters.post(`${BASE_ENDPOINT}`, authCheck, adminCheck, foodHandlers.postFood);
 foodRouters.get(`${BASE_ENDPOINT}`, authCheck, foodHandlers.getFoods);
 foodRouters.get(`${BASE_ENDPOINT}/:id`, authCheck, foodHandlers.getFood);
-foodRouters.put(`${BASE_ENDPOINT}/:id`, authCheck, foodHandlers.putFood);
-foodRouters.delete(`${BASE_ENDPOINT}/:id`, authCheck, foodHandlers.deleteFood);
+foodRouters.put(`${BASE_ENDPOINT}/:id`, authCheck, adminCheck, foodHandlers.putFood);
+foodRouters.delete(`${BASE_ENDPOINT}/:id`, authCheck, adminCheck, foodHandlers.deleteFood);
